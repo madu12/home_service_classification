181a4100fb66a069919dd1a6bea6d7dbbc3564a0
madu12 <sameeramadushan0812@gmail.com>
2024-10-13 23:49:42 +0530

    update port

--- app.py
+++ app.py
@@ -93,5 +93,5 @@ def confirm():
 
 # Run the app
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 3000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port, debug=True, use_reloader=True)
